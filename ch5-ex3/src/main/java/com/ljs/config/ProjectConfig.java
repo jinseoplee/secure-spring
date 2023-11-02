139d9344eb5340aa69313422e0cea41721555987
jinseoplee <jjjp9966@gmail.com>
2023-11-02 23:49:14 +0900

    add ch5-ex3

---
+++ ch5-ex3/src/main/java/com/ljs/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package com.ljs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
+        });
+        http.authorizeRequests().anyRequest().authenticated();
+        return http.build();
+    }
+}
