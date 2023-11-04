52ec527614abeea62053c5b590c62bfd005a5b55
jinseoplee <jjjp9966@gmail.com>
2023-11-04 17:41:00 +0900

    add ch7-ex2

---
+++ ch7-ex2/src/main/java/com/ljs/config/ProjectConfig.java
@@ -0,0 +1,57 @@
+package com.ljs.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var manager = new InMemoryUserDetailsManager();
+
+        // 첫 번째 사용자 ljs는 READ 권한을 가짐
+        var user1 = User.withUsername("ljs")
+                .password("1234")
+                .authorities("READ")
+                .build();
+
+        // 두 번째 사용자 cyj은 WRITE 권한을 가짐
+        var user2 = User.withUsername("cyj")
+                .password("1234")
+                .authorities("READ", "WRITE", "DELETE")
+                .build();
+
+        // 사용자는 UserDetailsService에 의해 추가되고 관리된다.
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        // 사용자에게 읽기 권한이 있어야 하지만 삭제 권한은 없어야 함을 나타낸다.
+        String expression = "hasAuthority('READ') and !hasAuthority('DELETE')";
+
+        http.authorizeRequests()
+                .anyRequest()
+                .access(expression);
+
+        return http.build();
+    }
+}
